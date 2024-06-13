c4e479bfa53b331b82aaaf548f6874da78b18949
iot <anylife@gmail.com>
2024-06-13 18:08:57 +0800

    DB升级，为数据迁移做准备

--- app/src/main/java/com/ai/face/search/FaceSearch1NActivity.java
+++ app/src/main/java/com/ai/face/search/FaceSearch1NActivity.java
@@ -74,11 +74,10 @@ public class FaceSearch1NActivity extends AppCompatActivity {
         // 2.各种参数的初始化设置
         SearchProcessBuilder faceProcessBuilder = new SearchProcessBuilder.Builder(getApplication())
                 .setLifecycleOwner(this)
-                .setNeedMultiValidate(false)      //是否需要筛选结果防止误识别，需要硬件CPU配置高，Android 8+
-                .setThreshold(0.88f)              //阈值设置，范围限 [0.8 , 0.95] 识别可信度，也是识别灵敏度
+                .setThreshold(0.85f)              //阈值设置，范围限 [0.8 , 0.95] 识别可信度
                 .setNeedNirLiveness(false)        //是否需要红外活体能力，只有1:N VIP 有
                 .setNeedRGBLiveness(false)        //是否需要普通RGB活体检测能力，只有1:N VIP 有
-                .setLicenceKey("yourLicense")     //合作的VIP定制客户群体需要
+                .setNeedMultiValidate(false)      //是否需要筛选结果防止误识别，需要硬件CPU配置高，Android 8+
                 //增删改人脸 参考@FaceImageEditActivity 中的方式，需要使用SDK 中的API 进行操作不能直接插入图片
                 .setFaceLibFolder(CACHE_SEARCH_FACE_DIR)  //内部存储目录中保存N 个人脸图片库的目录
                 .setImageFlipped(cameraLens == CameraSelector.LENS_FACING_FRONT) //手机的前置摄像头imageProxy 拿到的图可能左右翻转
